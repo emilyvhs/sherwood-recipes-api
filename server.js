1e64e5b430071aa394bba1d3da119470caafae67
Emily Sherwood <emily.sherwood@loucoll.ac.uk>
2026-06-23 13:12:18 +0100

    Setting up project

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const connectToDB = require('./database/db');
+
+const app = express();
+
+const port = process.env.PORT;
+
+connectToDB();
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is connected at ${port}`);
+});
