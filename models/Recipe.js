3da937f5d65ad1bb3b003daf522478b702ff5b67
Emily Sherwood <emily.sherwood@loucoll.ac.uk>
2026-06-23 13:43:25 +0100

    Creating RecipeSchema

---
+++ models/Recipe.js
@@ -0,0 +1,12 @@
+const mongoose= require('mongoose');
+
+const RecipeSchema = new mongoose.Schema({
+    name: String,       
+    portions: Number,    
+    ingredients: Array,
+    quantities: Array,
+    chefNames: Array,
+    recipeLocation: String 
+});
+
+module.exports = mongoose.model('Recipe', RecipeSchema);
